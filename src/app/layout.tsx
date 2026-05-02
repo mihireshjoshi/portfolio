df7f50da1e0e4adbce852c1087a87103768e3745
mihireshjoshi <mihireshsjoshi@gmail.com>
2026-05-03 03:08:24 +0530

    Speed Insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'Mihiresh Joshi — The Portfolio Chronicle',
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <SpeedInsights />
         <Navbar />
         <main>{children}</main>
         <Footer />
